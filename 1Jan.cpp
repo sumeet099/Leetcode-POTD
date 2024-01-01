4582004d7f47697ee31ebcd92d28075b95e2855f
Sumeet Ubale <91785353+sumeet099@users.noreply.github.com>
2024-01-01 11:18:28 +0530

    1Jan.cpp

--- 1Jan.cpp
+++ 1Jan.cpp
@@ -1,1 +1,35 @@
-1Jan.cpp
+//https://leetcode.com/problems/assign-cookies/description/
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        
+        multiset<int> m;
+        for(auto x: s){
+            m.insert(x);
+        }
+
+        sort(g.begin(),g.end());
+
+        int ans =0;
+
+        for(auto x: g){
+
+            auto it = m.lower_bound(x);
+
+            if(it==m.end()){
+                break;
+            }
+            m.erase(it);
+            ans+=1;
+        }
+
+        return ans;
+    }
+};
+
+// Time Complexity : O(n*logn)  
+//  -- log(n) operation for binary search & erasing element from set 
+//  -- for sorting also (no need but it's okay bcz any way TC is nlogn)
+
+// Space Complexity : O(n)  
