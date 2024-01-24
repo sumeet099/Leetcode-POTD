4f31ba304f10a4f4c3e211a45f57c506e0e5a2b3
Sumeet Ubale <91785353+sumeet099@users.noreply.github.com>
2024-01-24 13:29:54 +0530

    Add files via upload
    24 Jan 2024 Leetcode Daily Solution

---
+++ 24Jan2024.cpp
@@ -0,0 +1,47 @@
+//https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/description/
+//logic - depth first seach 
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root,vector<int>&v,int &ans){
+        if(root==NULL)return;
+        if(root!=NULL && root->left==NULL && root->right==NULL ){
+            v[root->val]++;
+            int odd =0;
+            int even = 0;
+            for(int i = 0; i<10; i++){
+                if(v[i] % 2)odd++;
+                else even+=v[i];
+            }
+            if(odd <=1)ans++;
+            v[root->val]--;
+            return;
+        }
+        
+        v[root->val]++;
+
+        dfs(root->left,v,ans);
+        
+        dfs(root->right,v,ans);
+
+        v[root->val]--;
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        vector<int> v(10,0);
+        int ans = 0;
+        dfs(root,v,ans);
+        return ans;
+    }
+};
